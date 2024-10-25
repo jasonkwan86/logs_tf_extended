dc28ca2f63132b754236505ee334666200a6b3df
Jason Kwan <kwan.jason2001@gmail.com>
2024-10-24 22:02:25 -0400

    circumvent reloading + refactor functions (1.2.4)

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # logs_tf_extended
- Firefox addon that adds ETF2L and RGL name and player division info to played games on logs.tf
+ [Firefox](https://addons.mozilla.org/en-CA/firefox/addon/logs_tf_extended/)/[Chrome](https://chromewebstore.google.com/detail/logstfextended/jcgpbbalnjcjedofegcdmeidnllgmapn) addon that adds ETF2L and RGL name and player division info to played games on [logs.tf](https://logs.tf/)
+
+
+## Notes
+The `"browser_specific_settings"` property must be deleted in the `manifest.json` file to test the unpacked extension on firefox
--- background.js
+++ background.js
@@ -1,57 +1,61 @@
 const isFirefox = typeof browser !== "undefined";
 const currentBrowser = isFirefox ? browser : chrome;
-
 const timer = (ms) => new Promise((res) => setTimeout(res, ms));
 
-async function GetRGLPastTeams(steamID) {
+const checkForErrors = (response) => {
+  if (response.status === 429) {
+    console.log("Rate limited for the following request");
+    console.log(response);
+    return true;
+  }
+  if (!response.ok) {
+    return true;
+  }
+
+  return false;
+}
+
+const getRGLPastTeams = async (steamID) => {
   const uri = `https://api.rgl.gg/v0/profile/${steamID}/teams`;
 
   await timer(1500);
-
   const response = await fetch(uri);
+  if (checkForErrors(response)) return;
+
   return await response.json();
 }
 
-async function GetRGLProfile(steamID) {
+const getRGLProfile = async (steamID) => {
   const uri = `https://api.rgl.gg/v0/profile/${steamID}`;
 
   await timer(1500);
-
   const response = await fetch(uri);
+  if (checkForErrors(response)) return;
 
-  if (!response.ok) {
-    console.log(response);
-    return;
-  }
   return await response.json();
-}
+};
 
-async function GetETF2LName(steamID) {
+const GetETF2LProfile = async (steamID) => {
   const uriETF2L = `https://api-v2.etf2l.org/player/${steamID}`;
 
   await timer(500);
-
   const response = await fetch(uriETF2L);
-
-  if (!response.ok) {
-    console.log(response);
-    return;
-  }
+  if (checkForErrors(response)) return;
 
   return await response.json();
-}
+};
 
-async function GetAllData(steamID, messageType) {
+const getAllData = async (steamID, messageType) => {
   let data;
   if (messageType === "rgl_profile") {
-    data = await GetRGLProfile(steamID);
+    data = await getRGLProfile(steamID);
   } else if (messageType === "etf2l_profile") {
-    data = await GetETF2LName(steamID);
+    data = await GetETF2LProfile(steamID);
   } else if (messageType === "rgl_past_teams") {
-    data = await GetRGLPastTeams(steamID);
+    data = await getRGLPastTeams(steamID);
   }
   return data;
-}
+};
 
 currentBrowser.runtime.onInstalled.addListener(async () => {
   await currentBrowser.storage.local.set({ showRGL: true });
@@ -60,6 +64,6 @@ currentBrowser.runtime.onInstalled.addListener(async () => {
 });
 
 currentBrowser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  GetAllData(message.steamID, message.type).then((data) => sendResponse(data));
+  getAllData(message.steamID, message.type).then((data) => sendResponse(data));
   return true;
 });
